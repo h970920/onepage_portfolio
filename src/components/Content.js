816d0e40a6152317dcf2b73e079aefa48900993e
h970920 <h970920@naver.com>
2024-03-05 19:47:45 +0900

    컨텐츠에 아무말적어놓기

---
+++ src/components/Content.js
@@ -0,0 +1,25 @@
+// src/components/Content.js
+import React from "react";
+import "./Content.css";
+
+const Content = () => {
+  return (
+    <section className="content">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam
+        velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate
+        commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed
+        eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam
+        nec dui.
+      </p>
+      <p>
+        Curabitur ligula sapien, tincidunt non, euismod vitae, posuere
+        imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed
+        cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus
+        accumsan cursus velit.
+      </p>
+    </section>
+  );
+};
+
+export default Content;
