d9f3caa7354c38bd5dbe0a281e520dfa80c3aea7
h970920 <h970920@naver.com>
2024-03-05 20:16:50 +0900

    슬릭 맘에 안들어서 언인스톨

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import "./styles/App.css";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
-import Portfolio from "./components/Portfolio"; // Portfolio 컴포넌트 import
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Content />
-      <Portfolio /> {/* Portfolio 컴포넌트 추가 */}
       <Footer />
     </div>
   );
